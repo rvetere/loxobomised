8e2789da9f6d97655f46ff00f0cef0e80cb52a3b
Remo Vetere <remo.vetere@gmail.com>
2023-11-08 10:18:38 +0100

    Fixes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://dns.loxonecloud.com/504F94A1634D");
-  await page.type("input[type=text]", "Mieter");
+  await page.goto("https://dns.loxonecloud.com/");
+  await page.type("input[type=text]", "");
 
-  await page.type("input[type=password]", "2XYFxiKqn0");
+  await page.type("input[type=password]", "");
 
   await page.click("button[type=submit]");
   await page.waitForNavigation();
