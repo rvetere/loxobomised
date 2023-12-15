9439571331bd1717cacbecac2aced44637fdd161
Remo Vetere <remo.vetere@gmail.com>
2023-12-15 19:42:05 +0100

    Fixes

--- src/lib/getPageInPool.js
+++ src/lib/getPageInPool.js
@@ -1,6 +1,7 @@
 async function getPageInPool(pool, room) {
   const instance = pool.find((p) => p.getRoom() === room);
   const page = await instance.getInstance();
+  return page;
 }
 
 module.exports = { getPageInPool };
