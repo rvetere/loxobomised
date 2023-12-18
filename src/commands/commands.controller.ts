695b40759db2c827dff637b19eb6b631030b7e58
Remo Vetere <remo.vetere@gmail.com>
2023-12-18 18:06:27 +0100

    Lower ramp up

--- src/commands/commands.controller.ts
+++ src/commands/commands.controller.ts
@@ -44,8 +44,9 @@ export class CommandsController {
     }
     this.resetTimer = setTimeout(this.resetRequestCounter, 1000 * 4);
 
+    const randomDelay = Math.floor(Math.random() * 1000);
     return {
-      delay: (this.requestCounter[category] || 0) * 1800,
+      delay: (this.requestCounter[category] || 0) * 350 + randomDelay,
       formattedDate: `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`,
     };
   }
