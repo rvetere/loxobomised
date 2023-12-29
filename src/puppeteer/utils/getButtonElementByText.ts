da4aa2b14c2e9eb7a82bafb4ce53b6a373a4ef23
Remo Vetere <remo.vetere@gmail.com>
2023-12-29 13:02:17 +0100

    Fixes

--- src/puppeteer/utils/getButtonElementByText.ts
+++ src/puppeteer/utils/getButtonElementByText.ts
@@ -3,10 +3,8 @@ import type { ElementHandle, Page } from "puppeteer";
 export const getButtonElementByText = async (page: Page, text: string) => {
   const elements = await page.$x(`//div[@role='button']/div[contains(text(),'${text}')]`);
   if (elements.length) {
-    if (elements.length === 1) {
+    if (elements.length <= 2) {
       return elements[0] as ElementHandle<Element>;
-    } else if (elements.length === 2) {
-      return elements[1] as ElementHandle<Element>;
     }
     throw new Error(`Found more than two elements with text ${text}!`);
   }
